eba0899c191bf471383b37ce870364195990a73e
Breno Coll <collbreno@dcc.ufrj.br>
2023-07-11 01:53:50 -0300

    Creating settings.json

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from calendar_api import CalendarAPI
 from match_to_event import MatchToEvent
 from scraper import Scraper
+import json
 
-scraper = Scraper(team='Fluminense', url='https://br.soccerway.com/teams/brazil/fluminense-football-club/312/matches/')
-calendar = CalendarAPI(calendar_id='primary')
+file = open('./settings.json')
+data = json.load(file)
+team = data['team']
+matches_url = data['matches_url']
+calendar_id = data['calendar_id']
+
+scraper = Scraper(team=team, url=matches_url)
+calendar = CalendarAPI(calendar_id=calendar_id)
 conversor = MatchToEvent()
 
 for match in scraper.get_scheduled_matches():
