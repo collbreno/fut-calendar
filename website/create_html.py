c141f2dc41422f1f116f71522d9a84f42fdff161
Breno Coll <collbreno@dcc.ufrj.br>
2024-05-01 02:58:28 -0300

    adding firebase analytics

--- website/create_html.py
+++ website/create_html.py
@@ -13,6 +13,7 @@ class Item:
     image_url: str
     name: str
     link: str
+    id: str = ''
 
 def __get_calendar_url(calendar_id: str) -> str:
     return f'https://calendar.google.com/calendar/u/0/r?cid={calendar_id}'
@@ -21,22 +22,23 @@ def __get_image_url(soccerway_id: str) -> str :
     return f'https://secure.cache.images.core.optasports.com/soccer/teams/150x150/{soccerway_id}.png'
 
 
-def __generate_teams_html(refs: list[DocumentReference], path):
-    team_data = []
+def __generate_teams_html(competition_name, refs: list[DocumentReference], path):
+    teams = []
     for team_ref in refs:
         doc = team_ref.get().to_dict()
         name = doc['name']
         if doc.get('flag') is not None:
             name += f' ({doc.get('flag')})'
-        team_data.append({
-            'name': name,
-            'image_url':doc.get('image_url', __get_image_url(team_ref.id)),
-            'link': __get_calendar_url(doc['calendar_id'])
-        })
-    team_data.sort(key=lambda x: x['name'])
+        teams.append(asdict(Item(
+            name=name,
+            image_url=doc.get('image_url', __get_image_url(team_ref.id)),
+            link=__get_calendar_url(doc['calendar_id']),
+            id=team_ref.id
+        )))
+    teams.sort(key=lambda x: x['name'])
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('template_teams.html')
-    html_output = template.render(list=team_data)
+    html_output = template.render(list=teams, competition=competition_name)
 
     # Write HTML output to a file
     with open(f'{TEAMS_FOLDER}{path}.html', 'w', encoding='utf-8') as file:
@@ -53,13 +55,14 @@ if __name__ == '__main__':
     if not os.path.exists(TEAMS_FOLDER):
         os.mkdir(TEAMS_FOLDER)
 
+    all_teams = 'Todos os times'
     competition_data.append(asdict(Item(
         link='/teams/all',
-        name='Todos os times',
-        image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Soccerball.svg/2048px-Soccerball.svg.png'
+        name=all_teams,
+        image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Soccerball.svg/2048px-Soccerball.svg.png',
     )))
-    teams: list[DocumentReference] = db.collection('teams').list_documents()
-    __generate_teams_html(teams, 'all')
+    team_refs: list[DocumentReference] = db.collection('teams').list_documents()
+    __generate_teams_html(all_teams, team_refs, 'all')
 
     competitions: list[DocumentReference] = list(db.collection('competitions').list_documents())
 
@@ -68,9 +71,10 @@ if __name__ == '__main__':
         competition_data.append(asdict(Item(
             link=f'/teams/{competition_ref.id}',
             name=competition['name'],
-            image_url=competition['image_url']
+            image_url=competition['image_url'],
+            id=competition_ref.id,
         )))
-        __generate_teams_html(competition['teams'], competition_ref.id)
+        __generate_teams_html(competition['name'], competition['teams'], competition_ref.id)
 
 
     env = Environment(loader=FileSystemLoader('.'))
