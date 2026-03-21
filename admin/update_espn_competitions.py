1fecbc63c5988356c67fbc796181721065c4c97c
Breno Coll <collbreno@dcc.ufrj.br>
2026-03-20 22:41:37 -0300

    update admin

--- admin/update_espn_competitions.py
+++ admin/update_espn_competitions.py
@@ -1,19 +1,20 @@
 from services.espn_competition_creator import EspnCompetitionCreator
 from services.calendar_api import CalendarOAuthApi
 from firebase_admin import firestore, initialize_app
-
+import os
 
 if __name__ == '__main__':
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './python_admin_credentials.json'
     initialize_app()
     db = firestore.client()
     calendar_api = CalendarOAuthApi()
     creator = EspnCompetitionCreator(db=db, calendar_api=calendar_api)
     creator.update(
-        slug='bra.1', 
+        slug='fifa.world', 
         create_calendar=True,
-        create_teams=True, 
-        create_maps=False,
-        use_mapper=False,
+        create_teams=False, 
+        create_maps=True,
+        use_mapper=True,
         flag=None, 
     )
     # creator.update('uefa.euro')
